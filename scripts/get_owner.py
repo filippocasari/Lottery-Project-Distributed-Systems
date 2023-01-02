1d3c20bb11885dd98e815296f385f67c9e08663c
filippocasari <fili9941@gmail.com>
2023-01-02 12:56:19 +0100

    fixing get owner files

--- scripts/get_owner.py
+++ scripts/get_owner.py
@@ -1,15 +1,13 @@
+import pathlib
 from brownie import accounts, config
 from web3 import Web3
-# from eth_utils import from_wei
 
 
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7546"))
 
-with open("utils/address.txt") as file_a:
-    contract_address = file_a.read()
-with open("utils/abi.json") as file_a:
-    abi = file_a.read()
-#with open("./contract_data/abi.json") as file_b:
+contract_address = pathlib.Path("utils/address.txt").read_text()
+abi = pathlib.Path("utils/abi.json").read_text()
+# with open("./contract_data/abi.json") as file_b:
 #    contract_abi = file_b.read()
 
 # initialising the contract instance
@@ -17,7 +15,4 @@ contract_instance = w3.eth.contract(address=contract_address, abi=abi)
 
 
 def main():
-    print("owner: " + str(contract_instance.functions.getOwner()))
-    
-
-    
+    print(f"owner: {str(contract_instance.functions.getOwner().call())}")
